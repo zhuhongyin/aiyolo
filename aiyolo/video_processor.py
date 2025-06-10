2343639ea3de2b2935adc79cb2c929b98fccc317
zhuhongyin <642719600@qq.com>
2025-06-10 09:48:56 +0800

    1

--- aiyolo/video_processor.py
+++ aiyolo/video_processor.py
@@ -19,9 +19,9 @@ class VideoProcessor:
     def initialize_camera(self):
         """初始化摄像头"""
         try:
-            cap = cv2.VideoCapture(0)  
+            # cap = cv2.VideoCapture(0)  
             # 使用 RTSP 流连接摄像头 ?tcp （添加参数tcp协议（默认udp协议）以提高稳定性）
-            # cap = cv2.VideoCapture("rtsp://admin:dtct123456@10.10.140.144", cv2.CAP_FFMPEG)
+            cap = cv2.VideoCapture("rtsp://admin:dtct123456@10.10.140.144", cv2.CAP_FFMPEG)
             if not cap.isOpened():
                 raise Exception("无法打开摄像头")
             # 禁用自动白平衡,提高检测的稳定性
